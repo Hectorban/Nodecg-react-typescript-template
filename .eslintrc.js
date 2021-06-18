2ebade68cb25c917690055d31075ca358fd1bedb
Hectorban <hectorban1234@gmail.com>
2021-06-18 15:47:56 -0500

    Disabled some rules that are resolved by parcel bundler

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,13 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    commonjs: true
   },
   extends: [
+    "plugin:@typescript-eslint/recommended",
     'plugin:react/recommended',
     'airbnb',
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -20,6 +23,18 @@ module.exports = {
     'react',
     '@typescript-eslint',
   ],
+  globals: {
+    'nodecg': true,
+    "NodeCG": true
+  },
   rules: {
+    "array-callback-return": "off",
+    "no-shadow": "off",
+    "import/no-unresolved": "off",
+    "import/extensions": "off",
+    "global-require": "off",
+    "no-use-before-define": "off",
+    "react/jsx-filename-extension": "off",
+    "@typescript-eslint/triple-slash-reference": "off"
   },
 };

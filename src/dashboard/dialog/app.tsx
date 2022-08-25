aee4a878ac021497df89f85fe52cceba1de49796
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2022-08-25 18:09:57 +0200

    Added pre-commit, deleted React from all tsx because it is not necessary with React 18

--- src/dashboard/dialog/app.tsx
+++ src/dashboard/dialog/app.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect } from 'react'
+import { ReactElement, useEffect } from 'react'
 
 function App(): ReactElement {
     useEffect(() => {
--- src/dashboard/panel/app.tsx
+++ src/dashboard/panel/app.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import { ReactElement } from 'react'
 
 function App(): ReactElement {
     return (
